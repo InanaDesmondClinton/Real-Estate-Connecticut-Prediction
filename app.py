ac5c07116dbe261d5b25561cf42dbe027fa330dc
InanaDesmondClinton <desmondclintoninani@gmail.com>
2025-03-31 20:24:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -133,8 +133,25 @@ elif method == "Enter Full Address":
 # User Inputs
 assessed_value = st.number_input("Assessed Value", min_value=0.0, format="%.2f")
 town = st.selectbox("Town", [
-    "Andover", "Ansonia", "Ashford", "Avon", "Barkhamsted", "Beacon Falls", 
-    # ... (rest of your towns list)
+    "Andover", "Ansonia", "Ashford", "Avon", "Barkhamsted", "Beacon Falls", "Berlin", "Bethany", "Bethel", "Bethlehem",
+    "Bloomfield", "Bolton", "Bozrah", "Branford", "Bridgeport", "Bridgewater", "Bristol", "Brookfield", "Brooklyn",
+    "Burlington", "Canaan", "Canterbury", "Canton", "Chaplin", "Cheshire", "Chester", "Clinton", "Colchester",
+    "Colebrook", "Columbia", "Cornwall", "Coventry", "Cromwell", "Danbury", "Darien", "Deep River", "Derby", "Durham",
+    "East Granby", "East Haddam", "East Hampton", "East Hartford", "East Haven", "East Lyme", "East Windsor",
+    "Eastford", "Ellington", "Enfield", "Essex", "Fairfield", "Farmington", "Franklin", "Glastonbury", "Goshen",
+    "Granby", "Greenwich", "Griswold", "Groton", "Guilford", "Haddam", "Hamden", "Hampton", "Hartford", "Hartland",
+    "Harwinton", "Hebron", "Kent", "Killingly", "Killingworth", "Lebanon", "Ledyard", "Lisbon", "Litchfield", "Lyme",
+    "Madison", "Manchester", "Mansfield", "Marlborough", "Meriden", "Middlebury", "Middlefield", "Middletown",
+    "Milford", "Monroe", "Montville", "Morris", "Naugatuck", "New Britain", "New Canaan", "New Fairfield",
+    "New Hartford", "New Haven", "New London", "New Milford", "Newington", "Newtown", "Norfolk", "North Branford",
+    "North Canaan", "North Haven", "North Stonington", "Norwalk", "Norwich", "Old Lyme", "Old Saybrook", "Orange",
+    "Oxford", "Plainfield", "Plainville", "Plymouth", "Pomfret", "Portland", "Preston", "Prospect", "Putnam",
+    "Redding", "Ridgefield", "Rocky Hill", "Roxbury", "Salem", "Salisbury", "Scotland", "Seymour", "Sharon",
+    "Shelton", "Sherman", "Simsbury", "Somers", "South Windsor", "Southbury", "Southington", "Sprague", "Stafford",
+    "Stamford", "Sterling", "Stonington", "Stratford", "Suffield", "Thomaston", "Thompson", "Tolland", "Torrington",
+    "Trumbull", "Union", "Vernon", "Voluntown", "Wallingford", "Warren", "Washington", "Waterbury", "Waterford",
+    "Watertown", "West Hartford", "West Haven", "Westbrook", "Weston", "Westport", "Wethersfield", "Willington",
+    "Wilton", "Winchester", "Windham", "Windsor", "Windsor Locks", "Wolcott", "Woodbridge", "Woodbury"
 ])
 property_type = st.selectbox("Property Type", [
     "Residential", "Single Family", "Condo", "Vacant Land", "Commercial", 
